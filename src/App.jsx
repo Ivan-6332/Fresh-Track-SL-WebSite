399ee65d78b7022a3bab15ee9ede5f11671f485f
Ivan-6332 <ivanakalanka@gmail.com>
2025-02-24 01:24:29 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Hero from './components/Hero';
 import WhatWeDo from './components/WhatWeDo';
 import Team from './components/Team';
@@ -26,38 +26,36 @@ const TeamPage = () => (
 
 const App = () => {
   return (
-    <Router>
-      <main className="relative min-h-screen overflow-x-hidden antialiased">
-        <Routes>
-          <Route path="/" element={
-            <MainLayout>
-              <HomePage />
-            </MainLayout>
-          } />
-          <Route path="/team" element={
-            <MainLayout>
-              <TeamPage />
-            </MainLayout>
-          } />
-          <Route path="/team/:id" element={
-            <MainLayout>
-              <TeamMember />
-            </MainLayout>
-          } />
-          <Route path="/stay-tuned" element={
-            <MainLayout>
-              <StayTuned />
-            </MainLayout>
-          } />
-          <Route path="/blog/:id" element={
-            <MainLayout>
-              <BlogPost />
-            </MainLayout>
-          } />
-        </Routes>
-      </main>
-    </Router>
-  )
-}
+    <main className="relative min-h-screen overflow-x-hidden antialiased">
+      <Routes>
+        <Route path="/" element={
+          <MainLayout>
+            <HomePage />
+          </MainLayout>
+        } />
+        <Route path="/team" element={
+          <MainLayout>
+            <TeamPage />
+          </MainLayout>
+        } />
+        <Route path="/team/:id" element={
+          <MainLayout>
+            <TeamMember />
+          </MainLayout>
+        } />
+        <Route path="/stay-tuned" element={
+          <MainLayout>
+            <StayTuned />
+          </MainLayout>
+        } />
+        <Route path="/blog/:id" element={
+          <MainLayout>
+            <BlogPost />
+          </MainLayout>
+        } />
+      </Routes>
+    </main>
+  );
+};
 
 export default App;
